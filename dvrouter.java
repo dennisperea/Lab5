1690391b0db0b4427925094a3799a45730d3d285
dennisperea <54592924+dennisperea@users.noreply.github.com>
2019-12-02 19:06:43 -0500

    Initial Input File Reading
    Initial Setup Stuff

--- dvrouter.java
+++ dvrouter.java
@@ -1,11 +1,27 @@
 import java.io.*; 
 import java.util.*;
+import java.util.Scanner; 
 public class dvrouter{
     /*
     * [ARGS] : {topologyFile, changesFile, messageFile}
     */
 
     public static void main(String [] args){
-        
+    // first check for number of arguments
+    if(args.length < 3) {
+        System.out.println("Error, usage: java ClassName topologyFile changesFile messageFile");
+	    System.exit(1);
+    }
+
+    try {
+        Scanner scan = new Scanner(new FileInputStream(args[0]));
+        while (scan.hasNextLine()) {
+            System.out.println(scan.nextLine());
+        }
+        scan.close();
+    } catch (Exception e){
+        System.out.println(e);
+    }
+
     }
 }
